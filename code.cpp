b032a916bc08f6ff925b3151a671ea4c31f06110
Berroteran25 <ianbg25@vt.edu>
2026-03-21 12:27:23 -0400

    v39

--- code.cpp
+++ code.cpp
@@ -398,9 +398,9 @@ void completeOrder(Cafe& cafe, Order& order) {
     }
 
     int item_index = static_cast<int>(order.menu_item_name);
-    if (item_index < 0 || item_index >= static_cast<int>(cafe.menu.size())) {
-        throw runtime_error("Negative value not allowed");
-    }
+    //if (item_index < 0 || item_index >= static_cast<int>(cafe.menu.size())) {
+    //    throw runtime_error("Negative value not allowed");
+    //}
 
     if (order.status == OrderStatus::Completed || order.status == OrderStatus::Abandoned) {
         throw runtime_error("Negative value not allowed");
